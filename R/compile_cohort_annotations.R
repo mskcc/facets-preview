1eb9711fbf6c0e3afb91dd052cf63e60046e3f7d
aprice <price0416@gmail.com>
2026-02-18 12:49:10 -0500

    Updated cohort compile to work with unadjusted/adjusted seg files.

--- R/compile_cohort_annotations.R
+++ R/compile_cohort_annotations.R
@@ -11,21 +11,21 @@
 #'   \item{\code{gene_level_file}:} {gene_level file used}
 #'   \item{\code{ccf_file}:} {ccf file used}
 #' }
-#'  
+#'
 #' @export compile_cohort_annotations
 compile_cohort_annotations <- function(samples_to_annotate, output_prefix, ncores=1) {
-  
+
   parallelize = F
-  if (ncores > 1) { 
-    library(doParallel) 
+  if (ncores > 1) {
+    library(doParallel)
     doParallel::registerDoParallel(cores = ncores)
     parallelize = T
   }
-  
+
   if (!("fit_to_use" %in% colnames(samples_to_annotate))) {
     samples_to_annotate$fit_to_use = NA
   }
-  
+
   samples_annotated <-
     samples_to_annotate %>%
     select(sample_id, sample_path, fit_to_use) %>%
@@ -35,22 +35,22 @@ compile_cohort_annotations <- function(samples_to_annotate, output_prefix, ncore
             sample_id = x$sample_id
             sample_path = x$sample_path
             fit_to_use = x$fit_to_use
-            
+
             if (!is.na(fit_to_use)) {
               return(fit_to_use)
             }
-            
+
             ## read from facets_qc.txt
             qc_file = paste0(sample_path, '/facets_qc.txt')
             review_file = paste0(sample_path, '/facets_review.manifest')
-            
+
             if (!file.exists(qc_file)| !file.exists(review_file)) { return() }
-            
+
             qc_runs = fread(qc_file) %>% filter(fit_name != 'Not selected')
-            
+
             if (nrow(qc_runs) == 0) { return() }
-            
-            fit = ''          
+
+            fit = ''
             if(any(qc_runs$is_best_fit)) {
               fit = qc_runs %>% filter(is_best_fit) %>% head(n=1)
             } else if ("default" %in% qc_runs$fit_name) {
@@ -58,33 +58,33 @@ compile_cohort_annotations <- function(samples_to_annotate, output_prefix, ncore
             } else {
               fit = qc_runs %>% head(n=1)
             }
-            
+
             reviews <-
               fread(review_file) %>%
               filter(fit_name == fit$fit_name) %>%
               arrange(desc(date_reviewed)) %>%
               select(review_status, fit_name, review_notes, reviewed_by, date_reviewed, use_only_purity_run, use_edited_cncf, reviewer_set_purity) %>%
               head(n=1)
-            
+
             fit <-
-              fit %>% 
-              left_join(reviews) 
-            
+              fit %>%
+              left_join(reviews)
+
             ####
             #### update purity and ploidy based on reviews;
             ####
-            fit <- 
-              fit %>% 
+            fit <-
+              fit %>%
               rowwise %>%
-              mutate(purity = ifelse(!is.na(reviewer_set_purity) & reviewer_set_purity != '', 
+              mutate(purity = ifelse(!is.na(reviewer_set_purity) & reviewer_set_purity != '',
                                      reviewer_set_purity,
-                                     ifelse(!is.na(use_only_purity_run) & use_only_purity_run, 
-                                            purity_run_Purity, 
+                                     ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
+                                            purity_run_Purity,
                                             hisens_run_Purity))) %>%
-              mutate(ploidy = ifelse(!is.na(use_only_purity_run) & use_only_purity_run, 
-                                     purity_run_Ploidy, 
+              mutate(ploidy = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
+                                     purity_run_Ploidy,
                                      hisens_run_Ploidy))
-            
+
             return (fit)
           }, .parallel = parallelize)
     )
@@ -92,74 +92,91 @@ compile_cohort_annotations <- function(samples_to_annotate, output_prefix, ncore
   samples_annotated <-
     samples_annotated %>%
     rowwise %>%
-    mutate(pfx = paste0(sample_path, '/', fit_name, '/', sample_id)) %>% 
-    mutate(arm_level_file = paste0(pfx, '.arm_level.txt'),
-           gene_level_file = paste0(pfx, '.gene_level.txt'),
-           ccf_file = paste0(pfx, '.ccf.maf'),
-           ccf_nonsignedout_file = paste0(pfx, '.nonsignedout.ccf.maf'),
-           cncf_file = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
-                              paste0(pfx, '_purity.cncf.txt'), paste0(pfx, '_hisens.cncf.txt')),
-           seg_file = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
-                              paste0(pfx, '_purity.seg'), paste0(pfx, '_hisens.seg'))) %>%
-    mutate(arm_level_file_exists = file.exists(arm_level_file),
-           gene_level_file_exists = file.exists(gene_level_file),
-           ccf_file_exists = file.exists(ccf_file),
-           ccf_nonsignedout_file_exists = file.exists(ccf_nonsignedout_file),
-           cncf_file_exists = file.exists(cncf_file),
-           seg_file_exists = file.exists(seg_file))
-  
+    mutate(pfx = paste0(sample_path, '/', fit_name, '/', sample_id)) %>%
+    mutate(
+      arm_level_file = paste0(pfx, '.arm_level.txt'),
+      gene_level_file = paste0(pfx, '.gene_level.txt'),
+      ccf_file = paste0(pfx, '.ccf.maf'),
+      ccf_nonsignedout_file = paste0(pfx, '.nonsignedout.ccf.maf'),
+      cncf_file = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
+                         paste0(pfx, '_purity.cncf.txt'), paste0(pfx, '_hisens.cncf.txt')),
+      seg_file_unadjusted = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
+                                   paste0(pfx, '_purity_diplogR.unadjusted.seg'), paste0(pfx, '_hisens_diplogR.unadjusted.seg')),
+      seg_file = ifelse(!is.na(use_only_purity_run) & use_only_purity_run,
+                        paste0(pfx, '_purity_diplogR.adjusted.seg'), paste0(pfx, '_hisens_diplogR.adjusted.seg'))
+    ) %>%
+    mutate(
+      arm_level_file_exists = file.exists(arm_level_file),
+      gene_level_file_exists = file.exists(gene_level_file),
+      ccf_file_exists = file.exists(ccf_file),
+      ccf_nonsignedout_file_exists = file.exists(ccf_nonsignedout_file),
+      cncf_file_exists = file.exists(cncf_file),
+      seg_file_unadjusted_exists = file.exists(seg_file_unadjusted),
+      seg_file_exists = file.exists(seg_file)
+    )
+
+
   write.table(samples_annotated, file=paste0(output_prefix, '.cohort.txt'), quote=F, row.names=F, sep='\t')
   cl <- makeCluster(ncores)
-  
+
   ccf_calls = rbindlist(parSapply(cl,
                                   (samples_annotated %>% filter(ccf_file_exists))$ccf_file,
-                                  fread, 
-                                  simplify = F, 
-                                  USE.NAMES=F), 
+                                  fread,
+                                  simplify = F,
+                                  USE.NAMES=F),
                         fill = T)
   write.table(ccf_calls, file=paste0(output_prefix, '.ccf.maf'), quote=F, row.names=F, sep='\t')
-  
+
   ccf_nonsignedout_calls = rbindlist(parSapply(cl,
                                   (samples_annotated %>% filter(ccf_nonsignedout_file_exists))$ccf_nonsignedout_file,
-                                  fread, 
-                                  simplify = F, 
-                                  USE.NAMES=F),  
+                                  fread,
+                                  simplify = F,
+                                  USE.NAMES=F),
                         fill = T)
   write.table(ccf_nonsignedout_calls, file=paste0(output_prefix, '.nonsignedout.ccf.maf'), quote=F, row.names=F, sep='\t')
-  
+
   arm_level_calls = rbindlist(parSapply(cl,
                                         (samples_annotated %>% filter(arm_level_file_exists))$arm_level_file,
-                                        fread, 
-                                        simplify = F, 
-                                        USE.NAMES=F), 
+                                        fread,
+                                        simplify = F,
+                                        USE.NAMES=F),
                               fill = T)
   write.table(arm_level_calls, file=paste0(output_prefix, '.arm_level.txt'), quote=F, row.names=F, sep='\t')
-  
+
   gene_level_calls = rbindlist(parSapply(cl,
                                         (samples_annotated %>% filter(gene_level_file_exists))$gene_level_file,
-                                        fread, 
-                                        simplify = F, 
-                                        USE.NAMES=F), 
+                                        fread,
+                                        simplify = F,
+                                        USE.NAMES=F),
                               fill = T)
   write.table(gene_level_calls, file=paste0(output_prefix, '.gene_level.txt'), quote=F, row.names=F, sep='\t')
-  
+
   seg_calls = rbindlist(parSapply(cl,
                                   (samples_annotated %>% filter(seg_file_exists))$seg_file,
-                                  fread, 
-                                  simplify = F, 
-                                  USE.NAMES=F), 
+                                  fread,
+                                  simplify = F,
+                                  USE.NAMES=F),
                         fill = T)
-  write.table(seg_calls, file=paste0(output_prefix, '.seg'), quote=F, row.names=F, sep='\t')
-  
+  write.table(seg_calls, file=paste0(output_prefix, '_diplogR.adjusted.seg'), quote=F, row.names=F, sep='\t')
+
+  seg_calls_unadjusted = rbindlist(parSapply(cl,
+                                             (samples_annotated %>% filter(seg_file_unadjusted_exists))$seg_file_unadjusted,
+                                             fread,
+                                             simplify = F,
+                                             USE.NAMES=F),
+                                   fill = T)
+  write.table(seg_calls_unadjusted, file=paste0(output_prefix, '_diplogR.unadjusted.seg'), quote=F, row.names=F, sep='\t')
+
+
   cncf_calls = rbindlist(parSapply(cl,
                                   (samples_annotated %>% filter(cncf_file_exists))$cncf_file,
-                                  fread, 
-                                  simplify = F, 
-                                  USE.NAMES=F), 
+                                  fread,
+                                  simplify = F,
+                                  USE.NAMES=F),
                         fill = T)
   write.table(cncf_calls, file=paste0(output_prefix, '.cncf.txt'), quote=F, row.names=F, sep='\t')
-  
-  
+
+
   return (samples_annotated)
 }
 
